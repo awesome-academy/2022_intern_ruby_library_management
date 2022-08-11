2eeae389771f3620e602561a690521c855ffeab0
caovanson06112001 <cao.van.son@sun-asterisk.com>
2022-08-11 14:42:39 +0700

    edit book

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,9 +1,10 @@
 class Admin::BooksController < AdminController
   before_action :logged_in_user
+  before_action :find_book, :find_array_category, except: %i(index new create)
 
   def index
     @pagy, @books = pagy Book.latest_books,
-                         items: Settings.max_page_user
+                         items: Settings.book.max_page_book
   end
 
   def new
@@ -22,6 +23,30 @@ class Admin::BooksController < AdminController
     end
   end
 
+  def show; end
+
+  def update
+    if @book.update book_params
+      @categories.destroy_all
+      @book.category build_params
+      flash[:message] = t "books.add_ss"
+      redirect_to admin_books_path
+    else
+      flash.now[:danger] = t "books.error"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @book.destroy
+      flash[:message] = t "books.delete_ss"
+      redirect_to admin_books_path
+    else
+      flash[:message] = t "books.delete_faild"
+    end
+    redirect_to admin_books_path
+  end
+
   private
 
   def book_params
@@ -38,6 +63,23 @@ class Admin::BooksController < AdminController
     arr[:category_book_ids].shift
     arr[:category_book_ids].map do |category_book_id|
       {category_book_id: category_book_id}
+    end
+  end
+
+  def find_book
+    @book = Book.find_by id: params[:id]
+    return if @book
+
+    store_location
+    redirect_to admin_books_path
+    flash[:danger] = t "book.find_error"
+  end
+
+  def find_array_category
+    @arr = Array.new
+    @categories = CategoriesBook.where book_id: @book.id
+    @categories.map do |item|
+      @arr.push item.category_book_id
     end
   end
 end
